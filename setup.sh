9a3ed6cdccbe7b646edee7fbabd68db5dbd4ab79
Johan van Soest <j.vansoest@maastrichtuniversity.nl>
2023-08-11 14:49:51 +0200

    Finished startup and shutdown scripts

--- setup.sh
+++ setup.sh
@@ -1,4 +1,35 @@
+VERSION_NODE=3.10.4
+VERSION_SERVER=$VERSION_NODE
+
 python -m venv ./venv
 source ./venv/bin/activate
 
-pip install -r requirements.txt
+pip install vantage6==$VERSION_NODE
+
+# Technically this is not needed, but makes life more easy ;-)
+docker pull harbor2.vantage6.ai/infrastructure/server:$VERSION_NODE
+docker pull harbor2.vantage6.ai/infrastructure/node:$VERSION_NODE
+
+########################
+# Central server setup
+########################
+
+# Start server
+cd infrastructure/server
+server_config=$(pwd)/wp3server.yaml
+vserver start --user -c $server_config --image harbor2.vantage6.ai/infrastructure/server:$VERSION_SERVER
+
+# Import server entities
+server_entities=$(pwd)/entities.yaml
+vserver import --user -c $server_config $server_entities
+
+cd ../../
+
+########################
+# InfoRing node setup
+########################
+
+# InfoRing node start
+cd infrastructure/inforing
+inforing_config=$(pwd)/inforing.yaml
+vnode start --user -c $inforing_config --image harbor2.vantage6.ai/infrastructure/node:$VERSION_NODE
---
+++ shutdown.sh
@@ -0,0 +1,13 @@
+source ./venv/bin/activate
+vnode stop --user -n inforing
+vserver stop --user -n wp3server
+deactivate
+
+docker volume rm $(docker volume ls -f name=vantage6 -q)
+# docker volume rm $(docker volume ls -f name=inforing -q)
+
+# Optional: delete images
+# docker rmi -f $(docker image ls --filter=reference='harbor*/*/*' -q)
+
+rm -R ~/Library/Application\ Support/vantage6/node/inforing
+rm -R ~/Library/Application\ Support/vantage6/server/wp3server
